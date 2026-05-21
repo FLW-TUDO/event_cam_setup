7d9e19e175d09d7dacd9e6a0c708ada9543e8169
Lokeshwaran Manohar <lokeshwaran.manohar@tu-dortmund.de>
2026-05-21 16:32:34 +0200

    add vicon pkg

--- run_camera_driver.sh
+++ run_camera_driver.sh
@@ -5,10 +5,11 @@
 # Inside the container:
 #   roslaunch RGB_event_cam_stereo.launch          # stream all cameras
 #   roslaunch record_wp1.launch exposure_us:=5000  # cameras only, no record
-#   /rgb_event_cam_system/record_wp1_docker.sh 5 30 my_label  # full recording
+#   /RGB_Event_cam_system/record_wp1_docker.sh 5 30 my_label  # full recording
 
 RGB_DIR="$HOME/event/RGB_Event_cam_system"
 BAGS_DIR="${1:-$HOME/bags}"
+VICON_IP="192.168.1.221"
 
 mkdir -p "$BAGS_DIR"
 
@@ -20,7 +21,8 @@ docker run -it --rm \
     --ipc=host \
     -e "DISPLAY=${DISPLAY:-}" \
     -e "QT_X11_NO_MITSHM=1" \
-    -e "ROS_PACKAGE_PATH=/rgb_event_cam_system:/catkin_ws/src:/opt/ros/noetic/share" \
+    -e "ROS_PACKAGE_PATH=/RGB_Event_cam_system:/catkin_ws/src:/opt/ros/noetic/share" \
+    -e "VICON_IP=${VICON_IP}" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v "/dev/bus/usb:/dev/bus/usb" \
     -v "/run/udev:/run/udev:ro" \
