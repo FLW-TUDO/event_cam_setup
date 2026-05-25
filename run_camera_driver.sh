dae8478f5f4285d6c2b23c7094a0376b735f1082
Lokeshwaran Manohar <lokeshwaran.manohar@tu-dortmund.de>
2026-05-25 13:09:55 +0200

    pass HOST_UID/HOST_GID into container so bags are owned by host user

--- run_camera_driver.sh
+++ run_camera_driver.sh
@@ -10,6 +10,8 @@
 RGB_DIR="$HOME/event/RGB_Event_cam_system"
 BAGS_DIR="${1:-$HOME/bags}"
 VICON_IP="192.168.2.221"
+HOST_UID="$(id -u)"
+HOST_GID="$(id -g)"
 
 mkdir -p "$BAGS_DIR"
 
@@ -23,6 +25,8 @@ docker run -it --rm \
     -e "QT_X11_NO_MITSHM=1" \
     -e "ROS_PACKAGE_PATH=/RGB_Event_cam_system:/catkin_ws/src:/opt/ros/noetic/share" \
     -e "VICON_IP=${VICON_IP}" \
+    -e "HOST_UID=${HOST_UID}" \
+    -e "HOST_GID=${HOST_GID}" \
     -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     -v "/dev/bus/usb:/dev/bus/usb" \
     -v "/run/udev:/run/udev:ro" \
